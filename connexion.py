eef0355519b3ccd49f0c8f5078c374f676e5d49c
Thomas DABRE <thomas.dabre@bts-malraux.net>
2014-10-08 17:17:18 +0200

    Optimisation du module connexion

--- connexion.py
+++ connexion.py
@@ -1,21 +1,10 @@
-#Import module Python
+#Import modules Python
 import sqlalchemy
 from accessBDD import loginBDD, passwordBDD, ipBDD, portBDD
 
-#Connexion a la BDD
-connexion = sqlalchemy.create_engine("postgresql://"+ loginBDD +":"+ passwordBDD +"@"+ ipBDD +":"+ portBDD +"/radio_libre")
-
+#Connexion BDD
+connexionPG = sqlalchemy.create_engine("postgresql://"+ loginBDD +":"+ passwordBDD +"@"+ ipBDD +":"+ portBDD +"/radio_libre")
 metadata = sqlalchemy.MetaData()
 
 #Construction de la table morceaux
 tableMorceaux = sqlalchemy.Table('morceaux', metadata, sqlalchemy.Column('titre', sqlalchemy.String))
-
-#Selection des des valeurs trouvées sur la BDD
-selection = sqlalchemy.select([tableMorceaux])
-
-#Execution de la requete
-morceaux = connexion.execute(selection)
-
-#Affichage des valeurs trouvées
-for i in morceaux:
-    print(i)
