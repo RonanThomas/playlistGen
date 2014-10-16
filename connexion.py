1b94aa6f9d74f6b374e872fe7e57b08c0b9e85ff
Ronan Legardinier <ronan.legardinier@gmail.com>
2014-10-16 23:18:58 +0200

    Modification des variables de connexion

--- connexion.py
+++ connexion.py
@@ -8,6 +8,7 @@ from globalConfig import LOGIN_BDD, PASS_BDD, DOMAIN_BDD, PORT_BDD
 
 #Connexion BDD
 connexionPG = sqlalchemy.create_engine("postgresql://"+ LOGIN_BDD +":"+ PASS_BDD +"@"+ DOMAIN_BDD +":"+ PORT_BDD +"/radio_libre")
+
 metadata = sqlalchemy.MetaData()
 
 #Construction de la table morceaux
