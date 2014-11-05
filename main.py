df23f6f5ca1087c8d19de063182899e3bc8270e9
Ronan Legardinier <ronan.legardinier@gmail.com>
2014-11-05 17:03:13 +0000

    Ecriture du fichier de logs

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ import verificationArguments
 import definitionCLI
 from globalConfig import ARGUMENTS_CLI
 from genPlaylist import downloadData, genPlaylist, writeFile, playlist
+from writeLogFile import writeLogs
 from color import Bold, Redbold
 
 #Déclaration du fichier de logs
 logging.basicConfig(filename="info.log", level=logging.DEBUG)
-logging.info("***** " + time.strftime('%d/%m/%y %H:%M:%S', time.localtime()) + " *****")
+logging.info("*****************************")
+logging.info("***** " + time.strftime('%d/%m/%y %H:%M:%S', time.localtime()) + " *****\n")
 
 Redbold("Génération de la playslit...")
 
@@ -36,9 +38,12 @@ Bold("Sélection des morceaux...")
 #Téléchargement des données
 downloadData(listeArgumentsCLI)
 #Génération de la playlist
-genPlaylist(listeArgumentsCLI)
+somme_duree = genPlaylist(listeArgumentsCLI)
 Bold("Génération terminée !")
 Bold("Ecriture du fichier de playlist...")
 #Ecriture du fichier de playlist
 writeFile(listeArgumentsCLI, playlist)
+Bold("Ecriture du fichier de logs...")
+#Ecriture du fichier de log
+writeLogs(listeArgumentsCLI, playlist, somme_duree)
 Redbold("Playlist enregistrée en tant '"+ listeArgumentsCLI.nom_playlist +"."+ listeArgumentsCLI.type_playlist +"'")
---
+++ writeLogFile.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+#Import modules Python
+import logging
+from gi.overrides.keysyms import minutes, seconds
+
+def writeLogs(listeArgumentsCLI, playlist, somme_duree):
+    playlistFileName = listeArgumentsCLI.nom_playlist +"."+ listeArgumentsCLI.type_playlist
+    logging.info("Nom de la playlist : " + playlistFileName + "\n")
+    
+    nombrePiste = len(playlist)
+    logging.info("Nombre de pistes : " + str(nombrePiste) + "\n")
+    
+    logging.info("Durée totale : " + convertToHours(somme_duree) + "\n\n")
+    
+def convertToHours(duree):
+    jours = 0
+    heures = 0
+    
+    secondes = duree % 60
+    minutes = int(duree / 60)
+    if(duree >= 3600):
+        heures = int(minutes / 60)
+        minutes = minutes % 60
+        if(duree >= 86400):
+            jours = int(heures / 24)
+            heures = heures % 60
+        
+    return str(jours) + "j " + str(heures) + "h " + str(minutes) + "m " + str(secondes) + "s"
