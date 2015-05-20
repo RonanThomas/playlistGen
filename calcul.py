443d8239a59ad295b4f5ea214e3242c70312955d
Legardinier Ronan <ronan.legardinier@gmail.com>
2015-05-20 16:23:25 +0200

    Documentation technque calcul.py

--- calcul.py
+++ calcul.py
@@ -3,7 +3,9 @@
 #Import modules Projet
 from globalConfig import ARGUMENTS_CLI
 
-'''Calcul de la somme des pourcentage pour tous les arguments'''
+## Calcul de la somme des pourcentage pour tous les arguments
+#  @param listeArgumentsCLI : Namespace
+#  @return somme_pourcent : Int
 def getSommePourcent(listeArgumentsCLI):
     somme_pourcent = 0
     for attribut in ARGUMENTS_CLI:
@@ -12,7 +14,10 @@ def getSommePourcent(listeArgumentsCLI):
                 somme_pourcent += pourcentage[1]
     return somme_pourcent
 
-'''Conversion des pourcentages en minutes'''
+
+## Convertie les pourcentage en minutes selon la durée totale de la plyalist
+#  @param listeArgumentsCLI : Namespace
+#  @return listeArgumentsCLI : Namespace
 def convertToMinute(listeArgumentsCLI):
     for attribut in ARGUMENTS_CLI:
         if getattr(listeArgumentsCLI, attribut) is not None:
