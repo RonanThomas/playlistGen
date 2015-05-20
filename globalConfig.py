6b1c4eea9589e67be83b98a559a9d029936052b0
Legardinier Ronan <ronan.legardinier@gmail.com>
2015-05-20 20:31:07 +0200

    Documentation technique de globalConfig.py

--- globalConfig.py
+++ globalConfig.py
@@ -14,18 +14,25 @@ if not (os.path.exists(".playlistGen.conf")):
     Redbold("/!\ Le fichier de configuration n'existe pas. Executez 'configure.py'. /!\\")
     sys.exit()
 else:
+    ## Chemin du fichier de configuration
     confFileName = ".playlistGen.conf"
+
+    ## Fichier de configuration
     confFile = open(confFileName, 'r')
 
 for ligne in confFile:
     if(ligne[0] != '#' and ligne[0] != '\n'):
         if(ligne.split("=")[0] == "LOGIN_BDD"):
+            ## Identifiant de la base de données
             LOGIN_BDD = ligne.split("=")[1][:-1]
         if(ligne.split("=")[0] == "PASS_BDD"):
+            ## Mot de passe de la base de données
             PASS_BDD = ligne.split("=")[1][:-1]
         if(ligne.split("=")[0] == "DOMAIN_BDD"):
+            ## Hôte du serveur de base de données
             DOMAIN_BDD = ligne.split("=")[1][:-1]
         if(ligne.split("=")[0] == "PORT_BDD"):
+            ## Port du serveur de base de données
             PORT_BDD = ligne.split("=")[1][:-1]
 
 
