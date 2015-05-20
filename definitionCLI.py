9437ed7810b17129e0f65419d83fd7f1cecbfc8e
Thomas DABRE <thomas.dabre@bts-malraux.net>
2015-05-20 15:05:06 +0200

    Ajout de la documentation

--- definitionCLI.py
+++ definitionCLI.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
+"""
+    Ce module définie la ligne de commande
+"""
+
 #Import modules Python
 import argparse
 import logging
 import sys
 
-'''Classe de vérification des arguments'''
 class appendCheckArgs(argparse.Action):
+    """Classe de vérification des arguments"""
+    
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
         if nargs == 2:
             super(appendCheckArgs, self).__init__(option_strings, dest, nargs=nargs, **kwargs)
@@ -30,14 +35,14 @@ class appendCheckArgs(argparse.Action):
 
 argsParser = argparse.ArgumentParser()
 
-'''"Déclaration des arguments positionnels'''
 def defArgumentsPositionnels():
+    """Déclaration des arguments positionnels"""
     argsParser.add_argument("duree_playlist", type=int, help="Durée totale de la playist en minutes")
     argsParser.add_argument("nom_playlist", help="Nom de sortie de la playist")
     argsParser.add_argument("type_playlist", help="Format de la playist", choices=['m3u', 'xspf', 'pls'])
 
-'''Déclaration des arguments optionnels'''
 def defArgumentsOptionnels():
+    """Déclaration des arguments optionnels"""
     argsParser.add_argument("-g", "--genre", action=appendCheckArgs, nargs=2, help="")
     argsParser.add_argument("-G", "--sousgenre", action=appendCheckArgs, nargs=2, help="")
     argsParser.add_argument("-a", "--artiste", action=appendCheckArgs, nargs=2, help="")
