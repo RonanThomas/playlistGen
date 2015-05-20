3bc35fa108fb75305e909b65d2b9ac4f4e55fcb8
Legardinier Ronan <ronan.legardinier@gmail.com>
2015-05-20 16:24:37 +0200

    Documentation technque color.py

--- color.py
+++ color.py
@@ -3,17 +3,23 @@
 #Import modules Python
 import os
 
+## Affiche un texte en rouge et en gras dans le terminal
+#  @param output : String
 def Redbold(output):
     os.system("tput bold")
     os.system("tput setaf 1")
     print(output)
     os.system("tput sgr 0")
-    
+   
+## Affiche un texte en gras dans le terminal
+#  @param output : String
 def Bold(output):
     os.system("tput bold")
     print(output)
     os.system("tput sgr 0")
     
+## Affiche un texte en rouge dans le terminal
+#  @param output : String
 def Red(output):
     os.system("tput setaf 1")
     print(output)
