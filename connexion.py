c2465ca7d61f34f08cb8921fdfdc0eedeaae01fc
Legardinier Ronan <ronan.legardinier@gmail.com>
2015-05-20 20:31:26 +0200

    Documentation technique de connexion.py

--- connexion.py
+++ connexion.py
@@ -8,12 +8,13 @@ import sqlalchemy
 #Import modules Projet
 from globalConfig import LOGIN_BDD, PASS_BDD, DOMAIN_BDD, PORT_BDD
 
-#Connexion BDD
+## Connexion à a base de donnée
 connexionPG = sqlalchemy.create_engine("postgresql://"+ LOGIN_BDD +":"+ PASS_BDD +"@"+ DOMAIN_BDD +":"+ PORT_BDD +"/radio_libre")
 
+## Meta-data de SQLAlchemy
 metadata = sqlalchemy.MetaData()
 
-#Construction de la table morceaux
+## Table morceaux
 tableMorceaux = sqlalchemy.Table('morceaux', metadata,
                                  sqlalchemy.Column('titre', sqlalchemy.String),
                                  sqlalchemy.Column('album', sqlalchemy.String),
