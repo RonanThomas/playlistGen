672a719540b2f186a3d3c665c7cc379c18d12143
Thomas DABRE <thomas.dabre@bts-malraux.net>
2015-06-12 10:25:19 +0200

    Modification de la requête pour 'artisteRegex'

--- genPlaylist.py
+++ genPlaylist.py
@@ -30,7 +30,7 @@ def downloadData(listeArgumentsCLI):
                 if(attribut == 'artiste'):
                     selection_morceaux = sqlalchemy.select([tableMorceaux]).where(tableMorceaux.c.artiste == argument[0])
                 if(attribut == 'artisteRegex'):
-                    selection_morceaux = sqlalchemy.select([tableMorceaux]).where(re.findall(argument[0], tableMorceaux.c.artiste))
+                    print(re.findall(argument[0], tableMorceaux.c.artiste))
                 if(attribut == 'album'):
                     selection_morceaux = sqlalchemy.select([tableMorceaux]).where(tableMorceaux.c.album == argument[0])
                 if(attribut == 'titre'):
